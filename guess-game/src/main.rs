e044b6f8669bee3854e0c615fd8839b91d183b0f
eufrat <lukzyw@gmail.com>
2023-12-20 17:42:57 +0100

    guess: loop

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -8,14 +8,19 @@ fn main() {
 
     let secret_number = rand::thread_rng().gen_range(1..=100);
 
-    let mut guess = String::new();
-    io::stdin().read_line(&mut guess).expect("failed");
+    loop {
+        let mut guess = String::new();
+        io::stdin().read_line(&mut guess).expect("failed");
 
-    let guess: i32 = guess.trim().parse().expect("provide number");
+        let guess: i32 = guess.trim().parse().expect("provide number");
 
-    match guess.cmp(&secret_number) {
-        Ordering::Less => println!("to small"),
-        Ordering::Greater => println!("to big"),
-        Ordering::Equal => println!("you win"),
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("to small"),
+            Ordering::Greater => println!("to big"),
+            Ordering::Equal => {
+                println!("you win");
+                break;
+            }
+        }
     }
 }
