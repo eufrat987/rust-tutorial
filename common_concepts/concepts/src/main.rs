f995cecca35cd3b9301e92e1e4e2185d34bf3e1f
eufrat <lukzyw@gmail.com>
2023-12-20 19:36:12 +0100

    common tuple

--- common_concepts/concepts/src/main.rs
+++ common_concepts/concepts/src/main.rs
@@ -14,6 +14,7 @@ fn main() {
 
     shadow();
     data_types();
+    tuple_example();
 }
 
 fn shadow() {
@@ -27,9 +28,16 @@ fn shadow() {
 }
 
 fn data_types() {
-    let i1: i8 = 127;
-    let i2: i16 = 32767;
-    let i3: i32 = 2147483647;
-    let i4: i64 = 9_223_372_036_854_775_807;
-    let i5: i128 = 2 ^ 128 - 1;
+    let _i1: i8 = 127;
+    let _i2: i16 = 32767;
+    let _i3: i32 = 2147483647;
+    let _i4: i64 = 9_223_372_036_854_775_807;
+    let _i5: i128 = 2 ^ 128 - 1;
+}
+
+fn tuple_example() {
+    let t = (1, false, 2.12);
+    let (x, y, z) = t;
+
+    println!("{x}, {y}, {z}");
 }
