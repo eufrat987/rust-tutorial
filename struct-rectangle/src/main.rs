2dc17685559f23c3ee8b6481fbfdf2f7d930bd0c
eufrat <lukzyw@gmail.com>
2023-12-21 20:49:32 +0100

    struct and rectangle

---
+++ struct-rectangle/src/main.rs
@@ -0,0 +1,17 @@
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+fn area(rect: Rectangle) -> u32 {
+    rect.width * rect.height
+}
+
+fn main() {
+    let rect = Rectangle {
+        width: 12,
+        height: 9,
+    };
+
+    println!("Area of rectangle is {}", area(rect));
+}
