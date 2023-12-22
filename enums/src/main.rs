89c4ef667085773413cd2640e7be05fa1290bd96
eufrat <lukzyw@gmail.com>
2023-12-23 00:33:45 +0100

    enum example with more params

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -3,7 +3,15 @@ enum IpAddrKind {
     V6(String),
 }
 
+enum Message {
+    Quit,
+    Move { x: i32, y: i32 },
+    Write(String),
+    ChangeColor(i32, i32, i32),
+}
+
 fn main() {
     let ip = IpAddrKind::V4(192, 168, 0, 1);
+    let msg1 = Message::Move { x: 23, y: 15 };
     println!("Hello, world!");
 }
