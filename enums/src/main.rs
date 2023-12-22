5ed6a7e4ea2e6dcd8968cc6058b19b9fe3dbbc42
eufrat <lukzyw@gmail.com>
2023-12-23 00:28:37 +0100

    enum example with params

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -1,9 +1,9 @@
 enum IpAddrKind {
-    V4,
-    V6,
+    V4(u8, u8, u8, u8),
+    V6(String),
 }
 
 fn main() {
-    let ip = IpAddrKind::V4;
+    let ip = IpAddrKind::V4(192, 168, 0, 1);
     println!("Hello, world!");
 }
