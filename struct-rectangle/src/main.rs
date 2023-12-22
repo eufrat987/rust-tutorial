577c205504a6be73d1c0ccd0dc403f2ddd702706
eufrat <lukzyw@gmail.com>
2023-12-22 19:34:06 +0100

    struct and methods

--- struct-rectangle/src/main.rs
+++ struct-rectangle/src/main.rs
@@ -12,6 +12,10 @@ impl Rectangle {
         self.height += 2;
         self
     }
+
+    fn can_hold(&self, rect: &Rectangle) -> bool {
+        rect.width <= self.width && rect.height <= self.height
+    }
 }
 
 fn main() {
@@ -19,10 +23,15 @@ fn main() {
         width: 12,
         height: 9,
     };
+    let rect3 = Rectangle {
+        width: 6,
+        height: 4,
+    };
 
     // auto dereference
     println!("Area of rectangle is {}", rect.area());
     println!("Rectangle height is {}", rect.height);
+    println!("can hold rect3?: {}", rect.can_hold(&rect3));
     // regular usage of function
     let rect2 = rect.example2();
     println!("Area of rectangle is {}", rect2.area());
